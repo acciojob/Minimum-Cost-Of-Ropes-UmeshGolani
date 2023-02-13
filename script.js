eb6bf1c9753eb3faca542ac1a66fd3bc3cd54fcb
UmeshGolani <35754561+UmeshGolani@users.noreply.github.com>
2023-02-13 12:18:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 
 function minimumCost() {
-    var a = document.getElementById("inputArr");
+    
     var res = 0;
+    var cost = 0;
     document.getElementById("result").innerHTML = res;
     var inputArray = document.getElementById("inputArr").value;
     inputArray = inputArray.split(',').map(Number);
     inputArray = inputArray.sort().reverse();
-    var a = inputArray.pop();
+    var res = inputArray.pop();
     for (let i = 1; i < inputArray.length; i++) {
-        res += a + inputArray[inputArray.length-i];
-        a = res;
+        
+        res += inputArray.pop();
+        cost += res;
+        inputArray[inputArray.length - i] = res;
         console.log(res);
     }
 
     
-    document.getElementById("result").innerHTML = res;
+    document.getElementById("result").innerHTML = cost;
 }
 
