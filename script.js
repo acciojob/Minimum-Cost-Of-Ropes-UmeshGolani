247c946d63f8deafe82c58dd916962b1c5ba50ff
UmeshGolani <35754561+UmeshGolani@users.noreply.github.com>
2023-02-13 10:42:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your code here
+function minimumCost(ropeLengths) {
+    ropeLengths.sort((a, b) => a - b);
+    let totalCost = 0;
+    let minCost = 0;
+    while (ropeLengths.length > 0) {
+        totalCost += ropeLengths.pop() ;
+        minCost = Math.max(minCost, totalCost);
+    }
+    return totalCost;
+}
+var a = [4, 2, 7, 6, 9];
+console.log(minimumCost(a));
